0f654c1bdae65af94f4eaad09f2fcc2b906dbde1
alvarooasencioordax-gif <alvarooasencioordax@gmail.com>
2026-04-16 20:03:52 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,15 @@
 const map = L.map('map',{zoomControl:false}).setView([36.4667, -6.1986], 15);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '© Google'
 }).addTo(map);
 
-L.marker([36.4667, -6.1986]).addTo(map)
-    .bindPopup('Humildad y Paciencia')
-    .openPopup();
+
+const miIconoRadar = L.divIcon({
+    className: 'punto-radar',
+    iconSize: [20, 20]
+});
+
+L.marker([36.4667, -6.1986], { icon: miIconoRadar }).addTo(map);
+
+
